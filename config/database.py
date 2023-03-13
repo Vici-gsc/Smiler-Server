4d66ec3aca979ca0864a21c7808c8a9b8733d1fa
박지윤 <jiyoon0043@ajou.ac.kr>
2023-03-13 21:43:46 +0900

    [Feat] set config to connect db

---
+++ config/database.py
@@ -0,0 +1,44 @@
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config.secret import db_info
+
+
+connector = Connector()
+
+
+def get_conn():
+    conn = connector.connect(
+            db_info['project_id'],
+            "pymysql",
+            user=db_info['user'],
+            password=db_info['password'],
+            db=db_info['database'],
+            ip_type=IPTypes.PUBLIC
+        )
+    return conn
+
+
+engine = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=get_conn,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ config/emotion_url.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from config.database import Base
+
+
+class UrlItem(Base):
+    __tablename__ = "emotion_url"
+
+    id = Column(Integer, primary_key=True, index=True)
+    emotion = Column(String)
+    url = Column(String)
